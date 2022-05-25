bb3e102cfcf7a9ca3205d57a1e2e18ff19eaf465
Ahsan Khan <nobody.ahsan@gmail.com>
2022-05-25 13:14:16 +0600

    update 6

--- addCourse.php
+++ addCourse.php
@@ -10,15 +10,8 @@
     }
 ?>
 
-
-<html>
-<head>
     <link rel="stylesheet" href="<?php echo CSS['modal.css']."?".time(); ?>">
     <script type="text/javascript" src="<?php echo JS['toggle-visibility.js']; ?>"></script>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
